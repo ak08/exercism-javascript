e8f366a539650274b7bdd346e90637dc851a37b3
Abhishek <akaushik079@gmail.com>
2019-03-27 15:23:42 +0530

    Updated solution to two-fer.js

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,5 +1,5 @@
 export const twoFer = (name) => {
-    if(name === '')
+    if(name === undefined)
         return 'One for you, one for me.';
     else
         return `One for ${name}, one for me.`;
